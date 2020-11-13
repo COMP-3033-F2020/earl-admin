5176c962cd5d3f8612bb7ecdc667d37171070fec
Mark Rabey <mark@markrabey.com>
2020-11-13 09:30:26 -0500

    fetch data with axios

--- src/components/earls/index.js
+++ src/components/earls/index.js
@@ -1,7 +1,16 @@
 import { Switch, Route, Link, useRouteMatch } from "react-router-dom";
+import axios from 'axios';
+import { useState } from "react";
 
 const Earls = () => {
   const match = useRouteMatch();
+  const [earls, setEarls] = useState([]);
+  
+  if (earls.length === 0) {
+    axios.get('http://localhost:3001/earls').then(({ data }) => {
+      setEarls(data);
+    });
+  }
 
   return (
     <div>
@@ -10,10 +19,19 @@ const Earls = () => {
       <Switch>
         <Route path={`${match.path}/:earlId`}>
           <div>Single Earl</div>
+          <Link to={match.path}>Back to list</Link>
         </Route>
         <Route path={match.path}>
           <div>
-            Earl List
+            {
+              earls.map((earl) => {
+                return (
+                  <div key={ earl._id }>
+                    <Link to={`${match.path}/${earl._id}`}>{ earl.target }</Link>
+                  </div>
+                )
+              })
+            }
           </div>
         </Route>
       </Switch>
